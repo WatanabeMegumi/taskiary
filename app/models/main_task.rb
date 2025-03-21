ec84027e34d52d9243ac013312dd3312c31e9034
WatanabeMegumi <wmegumi001@gmail.com>
2025-03-21 17:10:59 +0900

    バリデーションとアソシエーションの追加

--- app/models/main_task.rb
+++ app/models/main_task.rb
@@ -1,2 +1,7 @@
 class MainTask < ApplicationRecord
+  belongs_to :user
+
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :target_time, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :actual_time, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
 end
