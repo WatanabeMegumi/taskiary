5e5a3c66e398fb1653a5a61f51de543208bae16a
WatanabeMegumi <wmegumi001@gmail.com>
2025-03-27 00:59:43 +0900

    editアクションの追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_post, only: [:show]
+  before_action :set_post, only: [:show, :edit]
 
   def index
     @posts = Post.includes(:user).order(created_at: :desc)
@@ -68,6 +68,9 @@ class PostsController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
   end
 
   private
