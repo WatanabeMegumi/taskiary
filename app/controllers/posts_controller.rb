5896550cebd542519a45e845ec47c8f41fde6ed0
WatanabeMegumi <wmegumi001@gmail.com>
2025-03-28 02:49:21 +0900

    postsコントローラーの追記

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -68,6 +68,8 @@ class PostsController < ApplicationController
   end
 
   def show
+    @comment = Comment.new
+    @comments = @post.comments.includes(:user).order(created_at: :asc)
   end
 
   def edit
